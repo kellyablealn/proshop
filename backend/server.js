19556f82bcd5a2dd55da36896914616a282e125b
Kelly Azevedo Borges Leal Neves <kellyableal@gmail.com>
2021-04-13 11:11:25 -0300

    Added mongodb

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import {notFound, errorHandler} from "./middlewares/errorMiddleware.js";
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,30 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
+import {useDispatch, useSelector} from 'react-redux';
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+    const {loading, error, products} = productList;
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch]);
+
     return (
         <Fragment>
             <h1>Latest Products</h1>
-            <Row>
+            {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : (
+                <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product}/>
                     </Col>
                 ))}
             </Row>
+            )}
         </Fragment>
     )
 }
